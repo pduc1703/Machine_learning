bd52103a4bafdec943bd03410e6c8d6632f720d7
pduc1703 <duc.207ct27771@vanlanguni.vn>
2024-09-22 16:50:58 +0700

    xóa

--- Web/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const imagePreview = document.getElementById('imagePreview');
-    imagePreview.src = 'Lap1.png';
-    imagePreview.style.display = 'block'; 
-});
