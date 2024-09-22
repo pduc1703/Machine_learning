a32a602fb0fdc68f0b0529d1912b9ae19afe5d47
pduc1703 <duc.207ct27771@vanlanguni.vn>
2024-09-22 16:42:20 +0700

    sửa

--- Web/script.js
+++ Web/script.js
@@ -1,27 +1,5 @@
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-
-    const fileInput = document.getElementById('fileInput');
+document.addEventListener('DOMContentLoaded', function() {
     const imagePreview = document.getElementById('imagePreview');
-
-    if (fileInput.files && fileInput.files[0]) {
-        const file = fileInput.files[0];
-
-       
-        if (!file.type.startsWith('image/')) {
-            alert('Vui lòng chọn một tệp ảnh.');
-            return;
-        }
-
-        const reader = new FileReader();
-
-        reader.onload = function(e) {
-            imagePreview.src = e.target.result;
-            imagePreview.style.display = 'block'; 
-        }
-
-        reader.readAsDataURL(file);
-    } else {
-        alert('Vui lòng chọn một tệp ảnh.');
-    }
+    imagePreview.src = 'Lap1.png';
+    imagePreview.style.display = 'block'; 
 });
