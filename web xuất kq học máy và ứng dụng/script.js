72282dd87b44a5599064203806e57c8b19931e42
pduc1703 <duc.207ct27771@vanlanguni.vn>
2024-09-28 13:09:46 +0700

    Create script.js

---
+++ web xuất kq học máy và ứng dụng/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const imagePreview = document.getElementById('imagePreview');
+    imagePreview.src = 'lap2.1.png'; 
+    imagePreview.src = 'lap2.2.png';
+    imagePreview.src = 'lap2.3.png';
+    imagePreview.src = 'lap2.4.png';
+    imagePreview.src = 'lap2.5png';
+    imagePreview.src = 'lap2.6.png';
+    imagePreview.src = 'lap2.7.png';
+    imagePreview.style.display = 'block'; 
+});
