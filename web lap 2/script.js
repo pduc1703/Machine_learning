ee2264baf128703116179e64c3e9724a43069d22
pduc1703 <duc.207ct27771@vanlanguni.vn>
2024-09-28 13:14:27 +0700

    Update script.js

--- web lap 2/script.js
+++ web lap 2/script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const imagePreview = document.getElementById('imagePreview');
-    imagePreview.src = 'Lap1.png'; 
+    imagePreview.src = 'lap2.1.png'; 
+    imagePreview.src = 'lap2.2.png';
+    imagePreview.src = 'lap2.3.png';
+    imagePreview.src = 'lap2.4.png';
+    imagePreview.src = 'lap2.5png';
+    imagePreview.src = 'lap2.6.png';
+    imagePreview.src = 'lap2.7.png';
     imagePreview.style.display = 'block'; 
 });
