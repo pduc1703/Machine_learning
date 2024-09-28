c414c52394487b1d3a765c56a8b71037e96e03df
pduc1703 <duc.207ct27771@vanlanguni.vn>
2024-09-28 13:28:41 +0700

    Update script.js

--- web lap 2/script.js
+++ web lap 2/script.js
@@ -1,11 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const imagePreview = document.getElementById('imagePreview');
-    imagePreview.src = 'lap2.1.png'; 
-    imagePreview.src = 'lap2.2.png';
-    imagePreview.src = 'lap2.3.png';
-    imagePreview.src = 'lap2.4.png';
-    imagePreview.src = 'lap2.5png';
-    imagePreview.src = 'lap2.6.png';
-    imagePreview.src = 'lap2.7.png';
-    imagePreview.style.display = 'block'; 
+    const images = document.querySelectorAll('.imagePreview');
+    images.forEach(image => {
+        image.style.display = 'block'; 
+    });
 });
