99a1cd5efbc73c945a719ba92011521d5e71dc87
pduc1703 <duc.207ct27771@vanlanguni.vn>
2024-09-28 15:10:19 +0700

    Create script.js

---
+++ Lap1/web lap 1/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const imagePreview = document.getElementById('imagePreview');
+    imagePreview.src = 'Lap1.png'; 
+    imagePreview.style.display = 'block'; 
+});
