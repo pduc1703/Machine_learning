cd018fd184bccd23964de7e3fe592e702bfbb335
pduc1703 <duc.207ct27771@vanlanguni.vn>
2024-09-28 15:14:02 +0700

    Delete script.js

--- web lap 2/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const images = document.querySelectorAll('.imagePreview');
-    images.forEach(image => {
-        image.style.display = 'block'; 
-    });
-});
